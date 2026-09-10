4ffe452e52307b843b7f9e064a8169d4bc428e07
Shree Saravanan B <shreesaravanan24@gmail.com>
2026-09-11 00:38:08 +0530

    smart-banking-system

--- src/main/java/com/smartbanking/auth/controller/AuthController.java
+++ src/main/java/com/smartbanking/auth/controller/AuthController.java
@@ -2,8 +2,12 @@ package com.smartbanking.auth.controller;
 
 import com.smartbanking.auth.dto.AuthResponse;
 import com.smartbanking.auth.dto.LoginRequest;
+import com.smartbanking.auth.dto.RegisterRequest;
 import com.smartbanking.auth.service.AuthService;
+
 import jakarta.validation.Valid;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,11 +21,21 @@ public class AuthController {
         this.authService = authService;
     }
 
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(
+            @Valid @RequestBody RegisterRequest request) {
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(authService.register(request));
+    }
+
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(
             @Valid @RequestBody LoginRequest request) {
 
         return ResponseEntity.ok(
-                authService.login(request));
+                authService.login(request)
+        );
     }
 }
