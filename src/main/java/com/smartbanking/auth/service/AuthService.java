bbe92af506867157b78631d75c937e059a91c6e2
Shree Saravanan B <shreesaravanan24@gmail.com>
2026-09-11 00:33:59 +0530

    smart-banking-system

--- src/main/java/com/smartbanking/auth/service/AuthService.java
+++ src/main/java/com/smartbanking/auth/service/AuthService.java
@@ -2,8 +2,11 @@ package com.smartbanking.auth.service;
 
 import com.smartbanking.auth.dto.AuthResponse;
 import com.smartbanking.auth.dto.LoginRequest;
+import com.smartbanking.auth.dto.RegisterRequest;
 
 public interface AuthService {
+
+    AuthResponse register(RegisterRequest request);
 
     AuthResponse login(LoginRequest request);
 }
