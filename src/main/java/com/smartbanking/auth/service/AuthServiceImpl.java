f23a6d4064b5e9c4f0ded0f5107c83a722a7ccd1
Shree Saravanan B <shreesaravanan24@gmail.com>
2026-09-11 00:35:45 +0530

    smart-banking-system

--- src/main/java/com/smartbanking/auth/service/AuthServiceImpl.java
+++ src/main/java/com/smartbanking/auth/service/AuthServiceImpl.java
@@ -2,9 +2,15 @@ package com.smartbanking.auth.service;
 
 import com.smartbanking.auth.dto.AuthResponse;
 import com.smartbanking.auth.dto.LoginRequest;
+import com.smartbanking.auth.dto.RegisterRequest;
+import com.smartbanking.customer.entity.Customer;
+import com.smartbanking.customer.repository.CustomerRepository;
+import com.smartbanking.enums.Role;
 import com.smartbanking.security.JwtService;
+
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,15 +18,59 @@ public class AuthServiceImpl implements AuthService {
 
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
+    private final CustomerRepository customerRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public AuthServiceImpl(
             AuthenticationManager authenticationManager,
-            JwtService jwtService) {
+            JwtService jwtService,
+            CustomerRepository customerRepository,
+            PasswordEncoder passwordEncoder) {
 
-        this.authenticationManager =
-                authenticationManager;
-
+        this.authenticationManager = authenticationManager;
         this.jwtService = jwtService;
+        this.customerRepository = customerRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public AuthResponse register(RegisterRequest request) {
+
+        if (customerRepository.existsByEmail(request.getEmail())) {
+            throw new IllegalArgumentException(
+                    "Email already exists"
+            );
+        }
+
+        if (customerRepository.existsByPhone(request.getPhone())) {
+            throw new IllegalArgumentException(
+                    "Phone number already exists"
+            );
+        }
+
+        Customer customer = new Customer();
+
+        customer.setFullName(request.getFullName());
+        customer.setEmail(request.getEmail());
+        customer.setPhone(request.getPhone());
+
+        customer.setPassword(
+                passwordEncoder.encode(request.getPassword())
+        );
+
+        customer.setRole(Role.CUSTOMER);
+
+        Customer savedCustomer =
+                customerRepository.save(customer);
+
+        String token =
+                jwtService.generateToken(savedCustomer.getEmail());
+
+        return new AuthResponse(
+                token,
+                "Bearer",
+                savedCustomer.getEmail()
+        );
     }
 
     @Override
