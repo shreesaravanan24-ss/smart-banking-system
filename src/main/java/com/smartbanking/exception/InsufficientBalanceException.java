1f377e5c6a80853e5529f2902cf6f4fe8b998b29
Shree Saravanan B <shreesaravanan24@gmail.com>
2026-09-11 00:39:57 +0530

    smart-banking-system

--- src/main/java/com/smartbanking/exception/InsufficientBalanceException.java
+++ src/main/java/com/smartbanking/exception/InsufficientBalanceException.java
@@ -1,4 +1,8 @@
 package com.smartbanking.exception;
 
-public class InsufficientBalanceException {
+public class InsufficientBalanceException extends RuntimeException {
+
+    public InsufficientBalanceException(String message) {
+        super(message);
+    }
 }
