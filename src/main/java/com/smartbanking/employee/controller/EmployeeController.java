bbb1b7176b189a6ba80d2563afa47263d1fb3222
Shree Saravanan B <shreesaravanan24@gmail.com>
2026-09-12 00:09:38 +0530

    smart-banking-system

--- src/main/java/com/smartbanking/employee/controller/EmployeeController.java
+++ src/main/java/com/smartbanking/employee/controller/EmployeeController.java
@@ -2,13 +2,16 @@ package com.smartbanking.employee.controller;
 
 import com.smartbanking.account.dto.AccountResponse;
 import com.smartbanking.account.service.AccountService;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/employees")
+@PreAuthorize("hasAnyRole('ADMIN', 'BANK_EMPLOYEE')")
 public class EmployeeController {
 
     private final AccountService accountService;
@@ -20,18 +23,19 @@ public class EmployeeController {
     }
 
     @GetMapping("/accounts")
-    public ResponseEntity<List<AccountResponse>>
-    getAllAccounts() {
+    public ResponseEntity<List<AccountResponse>> getAllAccounts() {
 
         return ResponseEntity.ok(
-                accountService.getAllAccounts());
+                accountService.getAllAccounts()
+        );
     }
 
     @GetMapping("/accounts/{id}")
-    public ResponseEntity<AccountResponse>
-    getAccount(@PathVariable Long id) {
+    public ResponseEntity<AccountResponse> getAccount(
+            @PathVariable Long id) {
 
         return ResponseEntity.ok(
-                accountService.getAccountById(id));
+                accountService.getAccountById(id)
+        );
     }
 }
