eecf45c4855024a61e33bb23174eb143b96066b3
Shree Saravanan B <shreesaravanan24@gmail.com>
2026-09-12 00:09:12 +0530

    smart-banking-system

--- src/main/java/com/smartbanking/customer/controller/CustomerController.java
+++ src/main/java/com/smartbanking/customer/controller/CustomerController.java
@@ -3,9 +3,12 @@ package com.smartbanking.customer.controller;
 import com.smartbanking.customer.dto.CustomerRequest;
 import com.smartbanking.customer.dto.CustomerResponse;
 import com.smartbanking.customer.service.CustomerService;
+
 import jakarta.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,11 +19,14 @@ public class CustomerController {
 
     private final CustomerService customerService;
 
-    public CustomerController(CustomerService customerService) {
+    public CustomerController(
+            CustomerService customerService) {
+
         this.customerService = customerService;
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<CustomerResponse> createCustomer(
             @Valid @RequestBody CustomerRequest request) {
 
@@ -30,30 +36,37 @@ public class CustomerController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<CustomerResponse> getCustomer(
+    @PreAuthorize("hasAnyRole('CUSTOMER', 'ADMIN', 'BANK_EMPLOYEE')")
+    public ResponseEntity<CustomerResponse> getCustomerById(
             @PathVariable Long id) {
 
         return ResponseEntity.ok(
-                customerService.getCustomerById(id));
+                customerService.getCustomerById(id)
+        );
     }
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ADMIN', 'BANK_EMPLOYEE')")
     public ResponseEntity<List<CustomerResponse>> getAllCustomers() {
 
         return ResponseEntity.ok(
-                customerService.getAllCustomers());
+                customerService.getAllCustomers()
+        );
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasAnyRole('CUSTOMER', 'ADMIN', 'BANK_EMPLOYEE')")
     public ResponseEntity<CustomerResponse> updateCustomer(
             @PathVariable Long id,
             @Valid @RequestBody CustomerRequest request) {
 
         return ResponseEntity.ok(
-                customerService.updateCustomer(id, request));
+                customerService.updateCustomer(id, request)
+        );
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> deleteCustomer(
             @PathVariable Long id) {
 
