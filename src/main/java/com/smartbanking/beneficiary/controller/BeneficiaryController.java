3c60e078e7e268579d15e6943aaa90f6b357e10e
Shree Saravanan B <shreesaravanan24@gmail.com>
2026-09-11 23:45:53 +0530

    smart-banking-system

--- src/main/java/com/smartbanking/beneficiary/controller/BeneficiaryController.java
+++ src/main/java/com/smartbanking/beneficiary/controller/BeneficiaryController.java
@@ -3,9 +3,12 @@ package com.smartbanking.beneficiary.controller;
 import com.smartbanking.beneficiary.dto.BeneficiaryRequest;
 import com.smartbanking.beneficiary.dto.BeneficiaryResponse;
 import com.smartbanking.beneficiary.service.BeneficiaryService;
+
 import jakarta.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,28 +26,32 @@ public class BeneficiaryController {
     }
 
     @PostMapping
-    public ResponseEntity<BeneficiaryResponse>
-    addBeneficiary(
-            @Valid @RequestBody
-            BeneficiaryRequest request) {
+    @PreAuthorize("hasRole('CUSTOMER')")
+    public ResponseEntity<BeneficiaryResponse> addBeneficiary(
+            @Valid @RequestBody BeneficiaryRequest request) {
 
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(beneficiaryService
-                        .addBeneficiary(request));
+                .body(
+                        beneficiaryService
+                                .addBeneficiary(request)
+                );
     }
 
     @GetMapping("/customer/{customerId}")
+    @PreAuthorize("hasRole('CUSTOMER')")
     public ResponseEntity<List<BeneficiaryResponse>>
     getCustomerBeneficiaries(
             @PathVariable Long customerId) {
 
         return ResponseEntity.ok(
                 beneficiaryService
-                        .getCustomerBeneficiaries(customerId));
+                        .getCustomerBeneficiaries(customerId)
+        );
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('CUSTOMER')")
     public ResponseEntity<Void> deleteBeneficiary(
             @PathVariable Long id) {
 
