0f9a1d08f24b0a19672406ce64d705e661e8d18f
Shree Saravanan B <shreesaravanan24@gmail.com>
2026-09-12 00:08:32 +0530

    smart-banking-system

--- src/main/java/com/smartbanking/admin/controller/AdminController.java
+++ src/main/java/com/smartbanking/admin/controller/AdminController.java
@@ -4,14 +4,18 @@ import com.smartbanking.account.dto.AccountResponse;
 import com.smartbanking.admin.dto.AccountStatusRequest;
 import com.smartbanking.admin.service.AdminService;
 import com.smartbanking.customer.dto.CustomerResponse;
+
 import jakarta.validation.Valid;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin")
+@PreAuthorize("hasRole('ADMIN')")
 public class AdminController {
 
     private final AdminService adminService;
@@ -21,31 +25,31 @@ public class AdminController {
     }
 
     @GetMapping("/customers")
-    public ResponseEntity<List<CustomerResponse>>
-    getAllCustomers() {
+    public ResponseEntity<List<CustomerResponse>> getAllCustomers() {
 
         return ResponseEntity.ok(
-                adminService.getAllCustomers());
+                adminService.getAllCustomers()
+        );
     }
 
     @GetMapping("/accounts")
-    public ResponseEntity<List<AccountResponse>>
-    getAllAccounts() {
+    public ResponseEntity<List<AccountResponse>> getAllAccounts() {
 
         return ResponseEntity.ok(
-                adminService.getAllAccounts());
+                adminService.getAllAccounts()
+        );
     }
 
     @PutMapping("/accounts/{id}/status")
-    public ResponseEntity<AccountResponse>
-    updateAccountStatus(
+    public ResponseEntity<AccountResponse> updateAccountStatus(
             @PathVariable Long id,
-            @Valid @RequestBody
-            AccountStatusRequest request) {
+            @Valid @RequestBody AccountStatusRequest request) {
 
         return ResponseEntity.ok(
                 adminService.updateAccountStatus(
                         id,
-                        request.getStatus().name()));
+                        request.getStatus().name()
+                )
+        );
     }
 }
